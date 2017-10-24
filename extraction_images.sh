51ad64175faf0b420e10d13c430d0b7b807404ad
Nathalie Lyon-Caen <nathalie@nathalie-lyoncaen.com>
2017-10-24 08:11:02 +0200

    modif script

--- extraction_images.sh
+++ extraction_images.sh
@@ -1,5 +1,13 @@
 cd /some/folder/
+
+# découper
+ffmpeg -i 2°\ duan-1°part-Yangjia\ Michuan\ Taiji\ Quan.mp4 -vf "crop=400:300:120:30" test.mp4
+
+# extraire
 ffmpeg -i "*.mp4" -vf fps=1/4 extraction/out%04d.png
+
+# html
 ls extraction/*.png | sed 's#extraction#<img src="extraction#g' | sed 's#png#png">#g' > index.html
 
-# http://www.oodlestechnologies.com/blogs/Crop-and-scale-image-using-ffmpeg
+# pdf
+
