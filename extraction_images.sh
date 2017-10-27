c08189e808c224f7d30cd10b8cf02543526de287
Michael Douchin <mdouchin@3liz.com>
2017-10-27 10:16:42 +0200

    png -> jpg

--- extraction_images.sh
+++ extraction_images.sh
@@ -1,13 +1,13 @@
 cd /some/folder/
 
 # découper
-ffmpeg -i 2°\ duan-1°part-Yangjia\ Michuan\ Taiji\ Quan.mp4 -vf "crop=400:300:120:30" test.mp4
+ffmpeg -i video.mp4 -vf "crop=400:300:120:30" 1.mp4
 
 # extraire
-ffmpeg -i "*.mp4" -vf fps=1/4 extraction/out%04d.png
+ffmpeg -i "1.mp4" -vf fps=1/2 extraction/out%04d.jpg
 
 # html
-ls extraction/*.png | sed 's#extraction#<img style="width:300px" src="extraction#g' | sed 's#png#png">#g' > index.html
+ls extraction/*.jpg | sed 's#extraction#<img style="width:300px" src="extraction#g' | sed 's#jpg#jpg">#g' > index.html
 
 # pdf
 wkhtmltopdf --margin-left 5.0 --margin-right 5.0 --margin-top 2.0 --margin-bottom 2.0 index.html index.pdf
