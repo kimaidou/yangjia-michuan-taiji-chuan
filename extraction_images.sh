99053f962f32b82da738e48cc79466d8050af482
Nathalie Lyon-Caen <nathalie@nathalie-lyoncaen.com>
2017-10-29 09:33:08 +0100

    Change doc

--- extraction_images.sh
+++ extraction_images.sh
@@ -7,7 +7,8 @@ ffmpeg -i video.mp4 -vf "crop=400:300:120:30" 1.mp4
 ffmpeg -i "1.mp4" -vf fps=1/2 extraction/out%04d.jpg
 
 # html
-ls extraction/*.jpg | sed 's#extraction#<img style="width:300px" src="extraction#g' | sed 's#jpg#jpg">#g' > index.html
+ls extraction/*.jpg | sed 's#extraction#<img style="width:170px" src="extraction#g' | sed 's#jpg#jpg">#g' > index.html
 
 # pdf
-wkhtmltopdf --margin-left 5.0 --margin-right 5.0 --margin-top 2.0 --margin-bottom 2.0 index.html index.pdf
+ulimit -n 1024
+wkhtmltopdf --margin-left 5.0 --margin-right 5.0 --margin-top 5.0 --margin-bottom 2.0 index.html index.pdf
